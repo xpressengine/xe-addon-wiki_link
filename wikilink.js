e814357da079ccf71d03fab89d241cd925135183
flyskyko <sinsy200@gmail.com>
2013-03-22 09:01:41 +0000

    wiki_link addon: added a tag 1.7.0, merge branch 1.7.0 to trunk wiki module: added a tag 1.7.0, merge branch 1.7.0 to trunk
    git-svn-id: http://xe-wiki.googlecode.com/svn/trunk/addons/wiki_link@343 93e89afd-8622-bd68-7444-12f807e44a33

--- wikilink.js
+++ wikilink.js
@@ -1,4 +1,4 @@
-function openWikiLinkDialog()
+function openWikiLinkDialog(e)
 {
     var wikiLink = jQuery('#wikiLink');
     if ( wikiLink.length < 1 ) {
@@ -6,7 +6,7 @@ function openWikiLinkDialog()
             jQuery('<div id="wikiLink">Link Target : <input type="text" id="linktarget" class="inputTypeText" style="width:200px;" /></div>')
             .appendTo('body')
             .dialog({
-                title:'wiki Link', width:300, height:60, resizable:false,
+                title:'wiki Link', width:300, height:150, resizable:false,
                 modal: false, overlay: { opacity: 1, background: "#fff" },
                 buttons: { "add link": function() { setText(); jQuery(this).dialog("close"); }, "cancel": function() { jQuery(this).dialog("close"); } },
                 show: 'drop' /* , hide: 'explode' */
@@ -16,33 +16,37 @@ function openWikiLinkDialog()
     } else {
         wikiLink.dialog('open');
     }
+
+    e.stopPropagation();
+    e.preventDefault();
 }
 
 function setText() {
-    var target = xGetElementById('linktarget');
-    if(!target.value || target.value.trim() == '') return;
-    var text = target.value;
+    var target = jQuery('#linktarget');
+    if(!target.val() || target.val().trim() == '') return;
+    var text = target.val();
+    target.val('');
     text.replace(/&/ig,'&amp;').replace(/</ig,'&lt;').replace(/>/ig,'&gt;');
-    var url = request_uri.setQuery('mid',current_mid).setQuery('entry',text); 
+    var url = request_uri.setQuery('mid',current_mid).setQuery('entry',text);
     if(typeof(xeVid)!='undefined') url = url.setQuery('vid', xeVid);
     var link = "<a href=\""+url+"\" ";
     link += ">"+text+"</a>";
 
-    var iframe_obj = editorGetIFrame(1)
+    var iframe_obj = jQuery('iframe[id^="editor_iframe"]').get(0);
     editorReplaceHTML(iframe_obj, link);
     jQuery("#link").dialog("close");
 }
 
-function addShortCutForWiki() 
+function addShortCutForWiki()
 {
-    var iframe_obj = editorGetIFrame(1);
-    jQuery(iframe_obj.contentWindow.document).bind('keydown', "CTRL+SHIFT+SPACE", function(evt) { openWikiLinkDialog(); }); 
+    var iframe_obj = jQuery('iframe[id^="editor_iframe"]').get(0);
+    if(!iframe_obj) return;
+    jQuery(iframe_obj.contentWindow.document).bind('keydown', "CTRL+SHIFT+SPACE", openWikiLinkDialog);
     if(jQuery.os.Mac) {
-        jQuery(iframe_obj.contentWindow.document).bind('keydown', "ALT+SPACE", function(evt) { openWikiLinkDialog(); }); 
+        jQuery(iframe_obj.contentWindow.document).bind('keydown', "ALT+SPACE", openWikiLinkDialog);
     } else {
-        jQuery(iframe_obj.contentWindow.document).bind('keydown', "CTRL+SPACE", function(evt) { openWikiLinkDialog(); }); 
+        jQuery(iframe_obj.contentWindow.document).bind('keydown', "CTRL+SPACE", openWikiLinkDialog);
     }
-    jQuery(document).bind('keydown',"CTRL+SHIFT+SPACE", function(evt) {} );
 }
 
 jQuery(window).load( function() { addShortCutForWiki() } );
